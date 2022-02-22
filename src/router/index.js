7ab85e12da7af078e58cc5f68ddd919871674887
jax <jimmyjimmy66666@gmail.com>
2022-02-22 16:48:10 +0800

    week6

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,36 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes = [
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: HomeView,
+  // },
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    component: () => import("../views/FrontView.vue"),
+    children: [
+      {
+        path: "",
+        component: HomeView,
+      },
+      {
+        path: "products",
+        component: () => import("../views/ProductsView.vue"),
+      },
+      {
+        path: "cart",
+        component: () => import("../views/CartView.vue"),
+      },
+    ],
   },
   {
     path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () => import("../views/AboutView.vue"),
+  },
+  {
+    path: "/admin",
+    component: () => import("../views/DashboardView.vue"),
   },
 ];
 
